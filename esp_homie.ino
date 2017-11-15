0f441f67fe97220bc60cb95b1959d74d4204a98d
Yuri Volkov <yuri@madgeniuslab.com>
2017-11-16 00:58:58 +0200

    refactor + add mqtt

--- esp_homie.ino
+++ esp_homie.ino
@@ -1,50 +1,100 @@
+#include <string>
 #include <DHT.h>
+#include <ESP8266MQTTClient.h>
+#include <ESP8266WiFi.h>
 
-const int TEMPERATURE_INTERVAL = 300;
-unsigned long lastTemperatureSent = 0;
+class Sensor {
+  public:
+    Sensor()
+    :// dht(PIN_DHT22, DHT22),
+        dataReadTime(0) 
+    {
+    }
 
-const int PIN_DHT22 = D4;    // Broche - Pin DHT22
-DHT dht(PIN_DHT22, DHT22);
+    float temperature() {
+      return temp;
+    }
 
-//HomieNode temperatureNode("temperature", "temperature");
-//HomieNode humidityNode("humidity", "humidity");
+    float humidity() {
+      return humid;
+    }
 
-void loopHandler() {
-  //if (millis() - lastTemperatureSent >= TEMPERATURE_INTERVAL * 1000UL || lastTemperatureSent == 0) {
-    float t = dht.readTemperature();
-    float h = dht.readHumidity();
+    void read() {
+      if (millis() - dataReadTime >= TEMPERATURE_INTERVAL * 1000UL || dataReadTime == 0) {
+        temp = 0;//dht.readTemperature();
+        humid = 0;//dht.readHumidity();
+        dataReadTime = millis();
+      }
+    }
+  private:
+    const int TEMPERATURE_INTERVAL = 300;
+    const int PIN_DHT22 = D4;    // Broche - Pin DHT22
+
+    float temp;
+    float humid;
+    unsigned long dataReadTime;
+    //DHT dht;
+};
+
+class Notifier {
+  public:
+    Notifier() {
+      //topic, data, data is continuing
+      mqtt.onData([this](String topic, String data, bool cont) {
+        Serial.printf("Data received, topic: %s, data: %s\r\n", topic.c_str(), data.c_str());
+      });
     
-    // Affiche les mesures dans le journal - send values to the logger
-
-    //char buffer [50];
-    //int n;
-
-    Serial.println("");
-    //n = sprintf(buffer, "temperature %f", t);
-    Serial.print("Temperature: ");
-    Serial.println(t);
-    //n = sprintf(buffer, "humidity %f", h);
-    Serial.print("Humidity: ");
-    Serial.println(h);
-
-    //Serial.write(int(t));
-    //Serial << '\n';
+      mqtt.onSubscribe([this](int sub_id) {
+        Serial.printf("Subscribe topic id: %d ok\r\n", sub_id);
+      });
+      mqtt.onConnect([this]() {
+        Serial.printf("MQTT: Connected\r\n");
+        Serial.printf("Subscribe id: %d\r\n", mqtt.subscribe("/qos0", 0));
+      });
     
-    lastTemperatureSent = millis();
-  //}
-}
+      mqtt.begin(mqtt_url);
+    }
+
+    #define BUF_SIZE 5
+
+    void Notify(float h, float t) {
+      char buf[BUF_SIZE]; 
+      Serial.println("");
+
+      Serial.print("Temperature: ");
+      Serial.println(t);
+      sprintf(buf, "%f", t);
+      mqtt.publish("/temp", buf, 0, 0);
+
+      memset(buf, BUF_SIZE, 0);
+
+      Serial.print("Humidity: ");
+      Serial.println(h);
+      sprintf(buf, "%f", h);
+      mqtt.publish("/humid", buf, 0, 0);
+
+      delay(5000);
+    }
+  private:
+    MQTTClient mqtt;
+    const char* mqtt_url = "mqtt://test.mosquitto.org:1883";
+};
+
+
+Sensor s;
+Notifier n;
 
 void setup() {
   Serial.begin(115200);
+  WiFi.begin("ssid", "pass");
 
-  //Homie_setFirmware("demo-dht22", "1.0.0");
-  //Homie.setLoopFunction(loopHandler);
-
-  //Homie.setup();
-  //loopHandler();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 }
 
 void loop() {
-  loopHandler();
-  delay(1000);
+  s.read();
+  n.Notify(s.humidity(), s.temperature());
 }
