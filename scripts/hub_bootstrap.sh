b913d788dde62394d6cd2308c03f481382e84e62
Yuri Volkov <yuri@madgeniuslab.com>
2018-01-29 08:47:34 +0200

    make apt-get non-interactive, use USER env var instead of calling whoami

--- scripts/hub_bootstrap.sh
+++ scripts/hub_bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install python3-venv python3-dev python3-setuptools
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install python3-venv python3-dev python3-setuptools
 
 python3 -m venv hub
 cd hub
@@ -11,10 +11,9 @@ python3 -m pip install wheel
 python3 -m pip install homeassistant
 cd ..
 
-export CURRENT_USER=`whoami`
-cat home-assistant.service | sed -e s/homeassistant/$CURRENT_USER/g > home-assistant.service
-sudo cp home-assistant.service /etc/systemd/system/home-assistant@$CURRENT_USER.service
+cat home-assistant.service | sed -e s/homeassistant/$USER/g > home-assistant.service
+sudo cp home-assistant.service /etc/systemd/system/home-assistant@$USER.service
 
 sudo systemctl --system daemon-reload
-sudo systemctl enable home-assistant@$CURRENT_USER
-sudo systemctl start home-assistant@$CURRENT_USER
+sudo systemctl enable home-assistant@$USER
+sudo systemctl start home-assistant@$USER
